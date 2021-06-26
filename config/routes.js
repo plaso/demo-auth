596624bc8dcd03dab4890b3b2a95674c2c23bc20
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2021-06-26 12:10:02 +0200

    auth

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const usersController = require('../controllers/users.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.get('/', usersController.index);
-router.get('/register', usersController.register);
-router.post('/register', usersController.doRegister);
+router.get('/register', authMiddleware.isNotAuthenticated, usersController.register);
+router.post('/register', authMiddleware.isNotAuthenticated, usersController.doRegister);
+router.get('/login', authMiddleware.isNotAuthenticated, usersController.login);
+router.post('/login', authMiddleware.isNotAuthenticated, usersController.doLogin);
+router.get('/profile', authMiddleware.isAuthenticated, usersController.profile);
+router.post('/logout', authMiddleware.isAuthenticated, usersController.logout);
 
 module.exports = router;
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,13 @@
+const mongoose = require('mongoose');
 const User = require('../models/User.model');
 
 module.exports.index = (req, res, next) => {
+  console.log(req.session)
   User.find()
     .then(users => {
       res.render('index', {users});
     })
+    .catch((e) => next(e))
 }
 
 module.exports.register = (req, res, next) => {
@@ -16,4 +19,68 @@ module.exports.doRegister = (req, res, next) => {
     .then(() => {
       res.redirect('/')
     })
+    .catch((e) => {
+      if (e instanceof mongoose.Error.ValidationError) {
+        res.render('register', {
+          errors: e.errors,
+          user: {
+            email: req.body.email
+          }
+        })
+      } else if (e.code === 11000) {
+        res.render('register', {
+          errors: { email: 'There is already an account registered with this email' },
+          user: {
+            email: req.body.email
+          }
+        })
+      } else {
+        next(e)
+      }
+    })
+}
+
+module.exports.login = (req, res, next) => {
+  res.render('login')
+}
+
+module.exports.doLogin = (req, res, next) => {
+  // find by one usando el email
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        res.render('login', {
+          errorMessage: 'Email or password are invalid',
+          user: {
+            email: req.body.email
+          }
+        })
+      } else {
+        // comprobar la contraseña
+        return user.checkPassword(req.body.password)
+          .then((match) => {
+            if (match) {
+              req.session.currentUser = user
+              res.redirect('/profile')
+            } else {
+              res.render('login', {
+                errorMessage: 'Email or password are invalid',
+                user: {
+                  email: req.body.email
+                }
+              })
+            }
+          })
+      }
+    })
+    .catch(e => next(e))
+}
+
+module.exports.profile = (req, res, next) => {
+  res.render('profile')
+}
+
+module.exports.logout = (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
 }
